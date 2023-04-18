66eace8aa7a28d1bf4bb3f4a9974f0a18887319f
mithon0 <mithonhossen7244@gmail.com>
2023-04-18 15:45:37 +0600

    hghdufhs

--- src/components/Login/SignUp/SignUp.jsx
+++ src/components/Login/SignUp/SignUp.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import "./Login.css";
 import { Link } from 'react-router-dom';
 import { FcGoogle } from "react-icons/fc";
 import { BsGithub } from "react-icons/bs";
-const Login = () => {
+const SignUp = () => {
     return (
-       <div className='bg'>
-         <div className='login-container'>
-            <h1 className='title'>Login</h1>
+        <div>
+             <div className='bgg'>
+         <div className='login-containerr'>
+            <h1 className='title'>Sign Up Now</h1>
             <form >
                <div className='form'>
                <label className='lbl' htmlFor="email">Enter Your Email</label><br />
@@ -17,9 +17,13 @@ const Login = () => {
                <label className='lbl' htmlFor="password">Enter Your Password</label><br />
                 <input type="password" placeholder='Password' />
                </div>
+               <div>
+               <label className='lbl' htmlFor="password">confirm Your Password</label><br />
+                <input type="password" placeholder='Password' />
+               </div>
             </form>
             <button className='login-btn'>Login</button>
-            <p className='s'>New to Ema-john? <Link className='link' to="/signup">Create New Account</Link> </p>
+            <p className='s'><Link className='link' to="/login">Alrady have an Account?</Link> </p>
             <div className='line'>
             <div><hr className='b' /></div>
             <div className='c'><span className='d'>or</span> <hr className='b' /></div>
@@ -28,7 +32,8 @@ const Login = () => {
             <button className='google-btn'><BsGithub className='google-icon'/>Continue with GitHub</button>
         </div>
        </div>
+        </div>
     );
 };
 
-export default Login;
+export default SignUp;
